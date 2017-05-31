6a1a3022e5dff49d0a3540dcd7a894d78240f203
Lucas Holmquist <lholmqui@redhat.com>
2017-05-31 14:17:13 -0400

    Better image stream creation, better binary uploading

--- bin/cli.js
+++ bin/cli.js
@@ -3,6 +3,10 @@
 const nodeshiftConfig = require('../lib/nodeshift-config');
 const dockerArchiver = require('../lib/docker-archiver');
 const buildConfigurator = require('../lib/build-config');
+const imageStreamConfigurator = require('../lib/image-stream');
+const binaryBuild = require('../lib/binary-build');
+
+const currentDir = process.cwd();
 
 module.exports = function run (options) {
   // Run setup, this loads the config file
@@ -10,9 +14,6 @@ module.exports = function run (options) {
     // Once we have the config file, we do everything else
 
     // Create The Docker build archive
-    // create a temp/build directory
-    // Create a base Dockerfile
-    // tar up the users project with that generated Dockerfile
     return dockerArchiver.archiveAndTar().then(() => {
       console.log('Archive Created');
     }).then(() => {
@@ -21,8 +22,15 @@ module.exports = function run (options) {
     }).then((buildConfig) => {
       console.log('Build Config Created/Updated');
       // Check for an imagsestream, create or update if necesarry
+      return imageStreamConfigurator.createOrUpdateImageStream(config);
+    }).then((imageStream) => {
+      console.log('Image Stream Created/Updated');
+      // Start the build process
+      return binaryBuild(config, `${currentDir}/${dockerArchiver.DEFAULT_BUILD_LOCATION}/archive.tar`);
+    }).then(() => {
+      console.log('Uploaded');
     });
   }).catch((err) => {
-    console.log('Error:', err.message);
+    console.log('Error:', err);
   });
 };
