f97787a1c6b814b57863f89c60cf8ecb1c36ee01
Lance Ball <lball@redhat.com>
2017-07-11 19:01:03 -0400

    enhance: make logging nicer looking and consistent

--- bin/cli.js
+++ bin/cli.js
@@ -5,6 +5,7 @@ const dockerArchiver = require('../lib/docker-archiver');
 const buildConfigurator = require('../lib/build-config');
 const imageStreamConfigurator = require('../lib/image-stream');
 const binaryBuild = require('../lib/binary-build');
+const logger = require('../lib/common-log');
 
 const resourceLoader = require('../lib/resource-loader');
 const applyResources = require('../lib/apply-resources');
@@ -13,17 +14,12 @@ module.exports = async function run (options) {
   try {
     const config = await nodeshiftConfig(options);
     await dockerArchiver.archiveAndTar(config);
-    const buildConfig = await buildConfigurator.createOrUpdateBuildConfig(config);
-    console.log(`build configuration ${buildConfig.metadata.name} created/updated`);
-    const imageStream = await imageStreamConfigurator.createOrUpdateImageStream(config);
-    console.log(`application image stream ${imageStream.metadata.name} created/updated`);
+    await buildConfigurator.createOrUpdateBuildConfig(config);
+    await imageStreamConfigurator.createOrUpdateImageStream(config);
     await binaryBuild(config, `${config.projectLocation}/${dockerArchiver.DEFAULT_BUILD_LOCATION}/archive.tar`);
-    const dockerImageRepo = await imageStreamConfigurator.getDockerImageRepo(config);
     // should we do this?
-    config.dockerImageRepo = dockerImageRepo;
-    console.log(`docker image repository ${dockerImageRepo}`);
-    const resources = await resourceLoader(config);
-    await applyResources(config, resources);
+    config.dockerImageRepo = await imageStreamConfigurator.getDockerImageRepo(config);
+    await applyResources(config, await resourceLoader(config));
     return 'done';
   } catch (err) {
     console.error(err);
