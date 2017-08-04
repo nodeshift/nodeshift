8b83d08ed8ce6ff8063458efea4b966a4547acf7
Lucas Holmquist <lholmqui@redhat.com>
2017-08-04 11:50:59 -0400

    feat(nodeshift): pass new config loader options to the option config loader.
    fixes #66

--- test/nodeshift-config-test.js
+++ test/nodeshift-config-test.js
@@ -3,19 +3,19 @@
 const test = require('tape');
 const proxyquire = require('proxyquire');
 
-const nodeshiftConfig = proxyquire('../lib/nodeshift-config', {
-  'openshift-config-loader': () => {
-    return Promise.resolve({
-      context: {
-        namespace: 'test-namespace'
-      },
-      cluster: 'http://mock-cluster'
-    });
-  },
-  'openshift-rest-client': () => { return Promise.resolve({}); }
-});
-
 test('nodeshift-config basic setup', (t) => {
+  const nodeshiftConfig = proxyquire('../lib/nodeshift-config', {
+    'openshift-config-loader': () => {
+      return Promise.resolve({
+        context: {
+          namespace: 'test-namespace'
+        },
+        cluster: 'http://mock-cluster'
+      });
+    },
+    'openshift-rest-client': () => { return Promise.resolve({}); }
+  });
+
   const p = nodeshiftConfig().then((config) => {
     t.ok(config.projectLocation, 'projectLocation prop should be here');
     t.equal(config.projectLocation, process.cwd(), 'projectLocation prop should be cwd by default');
@@ -28,6 +28,18 @@ test('nodeshift-config basic setup', (t) => {
 });
 
 test('nodeshift-config other project location and nodeshiftDir', (t) => {
+  const nodeshiftConfig = proxyquire('../lib/nodeshift-config', {
+    'openshift-config-loader': () => {
+      return Promise.resolve({
+        context: {
+          namespace: 'test-namespace'
+        },
+        cluster: 'http://mock-cluster'
+      });
+    },
+    'openshift-rest-client': () => { return Promise.resolve({}); }
+  });
+
   const options = {
     projectLocation: '../examples/sample-project',
     nodeshiftDirectory: '.notnodeshift'
@@ -41,6 +53,18 @@ test('nodeshift-config other project location and nodeshiftDir', (t) => {
 });
 
 test('nodeshift-config no project Version', (t) => {
+  const nodeshiftConfig = proxyquire('../lib/nodeshift-config', {
+    'openshift-config-loader': () => {
+      return Promise.resolve({
+        context: {
+          namespace: 'test-namespace'
+        },
+        cluster: 'http://mock-cluster'
+      });
+    },
+    'openshift-rest-client': () => { return Promise.resolve({}); }
+  });
+
   const options = {
     projectLocation: '../examples/sample-project-no-version'
   };
@@ -52,12 +76,53 @@ test('nodeshift-config no project Version', (t) => {
 });
 
 test('nodeshift-config no package.json', (t) => {
+  const nodeshiftConfig = proxyquire('../lib/nodeshift-config', {
+    'openshift-config-loader': () => {
+      return Promise.resolve({
+        context: {
+          namespace: 'test-namespace'
+        },
+        cluster: 'http://mock-cluster'
+      });
+    },
+    'openshift-rest-client': () => { return Promise.resolve({}); }
+  });
+
   const options = {
     projectLocation: './not-here'
   };
 
   nodeshiftConfig(options).catch((err) => {
     t.equal(err.message, 'Cannot find module \'./not-here/package.json\'', 'Error Should be "\'Cannot find module \'./not-here/package.json\'\'"');
+    t.end();
+  });
+});
+
+test('nodeshift-config options for the config loader', (t) => {
+  const nodeshiftConfig = proxyquire('../lib/nodeshift-config', {
+    'openshift-config-loader': (options) => {
+      t.ok(options.configLocation, 'configLocation should be there');
+      t.ok(options.tryServiceAccount, 'tryServiceAccount should be there');
+      return Promise.resolve({
+        context: {
+          namespace: 'test-namespace'
+        },
+        cluster: 'http://mock-cluster'
+      });
+    },
+    'openshift-rest-client': () => { return Promise.resolve({}); }
+  });
+
+  const options = {
+    configLocation: '../examples/sample-project',
+    osl: {
+      tryServiceAccount: true
+    }
+  };
+
+  nodeshiftConfig(options).then((config) => {
+    t.equal(config.configLocation, '../examples/sample-project', 'configLocation prop should be changed');
+    t.ok(config.osl.tryServiceAccount, 'osl.tryServiceAccount should be there');
     t.end();
   });
 });
