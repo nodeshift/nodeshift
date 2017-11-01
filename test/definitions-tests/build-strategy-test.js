1466f0ced8a4f7e7be01474d5a1a679ff79921d9
Lucas Holmquist <lholmqui@redhat.com>
2017-11-01 15:17:33 +0000

    update: add the forcePull options to the build strategy.
    * from the cli specify the --build.forcePull flag to enable
    
     fixes #114

---
+++ test/definitions-tests/build-strategy-test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const test = require('tape');
+const buildStrategy = require('../../lib/definitions/build-strategy');
+
+test('default strategy', (t) => {
+  const result = buildStrategy();
+
+  t.equal(result.type, 'Source', 'default is Source type');
+  t.equal(result.sourceStrategy.from.name, 'bucharestgold/centos7-s2i-nodejs:latest', 'docker image should be latet BG gold image');
+  t.equal(result.sourceStrategy.forcePull, undefined, 'no forcePull by default');
+
+  t.end();
+});
+
+test('strategy with changed dockerTag', (t) => {
+  const result = buildStrategy({nodeVersion: '8.x'});
+  t.equal(result.sourceStrategy.from.name, 'bucharestgold/centos7-s2i-nodejs:8.x', 'docker image should be 8.x BG gold image');
+  t.end();
+});
+
+test('strategy with changed forcePull', (t) => {
+  const result = buildStrategy({forcePull: true});
+  t.equal(result.sourceStrategy.forcePull, true, 'forcePull on');
+  t.end();
+});
+
+test('strategy with changed forcePull to false', (t) => {
+  const result = buildStrategy({forcePull: false});
+  t.equal(result.sourceStrategy.forcePull, false, 'forcePull off');
+  t.end();
+});
