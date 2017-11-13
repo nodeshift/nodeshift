8d7fc29d1a7801b6b204b189c553eac43eb04161
Lucas Holmquist <lholmqui@redhat.com>
2017-11-13 18:40:57 +0000

    test(pod-template-spec): add unit test for pod-template-spec definition.

---
+++ test/definitions-tests/pod-template-spec-test.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const test = require('tape');
+const proxyquire = require('proxyquire');
+
+test('pod template spec test', (t) => {
+  const podtemplateSpec = proxyquire('../../lib/definitions/pod-template-spec', {
+    './pod-spec': (resource, config) => {
+      return resource;
+    }
+  });
+
+  const resource = {
+    spec: {
+      template: {}
+    }
+  };
+
+  const config = {
+    projectName: 'my project name',
+    projectVersion: '1.0.1'
+  };
+
+  const pts = podtemplateSpec(resource, config);
+  t.ok(pts.spec.template.metadata, 'should have a metadata prop');
+  t.ok(pts.spec.template.metadata.labels, 'should have a labels prop');
+  t.ok(pts.spec.template.metadata.labels.app, 'should have a labels.app prop');
+  t.equal(pts.spec.template.metadata.labels.provider, 'nodeshift', 'should have a provider label of nodeshift');
+  t.equal(pts.spec.template.metadata.labels.app, config.projectName, `should have a app label ${config.projectName}`);
+  t.equal(resource, pts, 'should be the same'); // Not thrilled about this mutation
+  t.end();
+});
+
+test('pod template spec test - template added', (t) => {
+  const podtemplateSpec = proxyquire('../../lib/definitions/pod-template-spec', {
+    './pod-spec': (resource, config) => {
+      return resource;
+    }
+  });
+
+  const resource = {
+    spec: {
+    }
+  };
+
+  const config = {
+    projectName: 'my project name',
+    projectVersion: '1.0.1'
+  };
+
+  const pts = podtemplateSpec(resource, config);
+  t.ok(pts.spec.template, 'should have a template prop');
+  t.end();
+});
