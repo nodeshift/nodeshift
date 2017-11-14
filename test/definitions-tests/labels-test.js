2eb96dd49007b62442ff3cc54dd3714b3f64f8cb
Lucas Holmquist <lholmqui@redhat.com>
2017-11-14 01:12:50 +0000

    test(labels): add unit test for lables definition.

---
+++ test/definitions-tests/labels-test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const test = require('tape');
+const labels = require('../../lib/definitions/labels');
+
+test('labels test', (t) => {
+  const l = labels({label: 'cool thing'});
+  t.equal(l.provider, 'nodeshift', 'nodeshift provider prop gets added');
+  t.equal(l.label, 'cool thing', 'orginal label is still there');
+  t.end();
+});
