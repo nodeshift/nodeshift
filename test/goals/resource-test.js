f4f3ac505ddcf00b9d41b7c0b18ef26ea5d7ec82
Lucas Holmquist <lholmqui@redhat.com>
2017-11-14 20:32:27 +0000

    test(goals/resource): add unit test for resource goal.

---
+++ test/goals/resource-test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const test = require('tape');
+const proxyquire = require('proxyquire');
+
+test('resource goal', (t) => {
+  const resourceGoal = proxyquire('../../lib/goals/resource', {
+    '../resource-loader': () => Promise.resolve([]),
+    '../enrich-resources': () => Promise.resolve([]),
+    '../resource-writer': () => Promise.resolve()
+  });
+
+  const rg = resourceGoal().then(() => {
+    t.pass('successful call');
+    t.end();
+  });
+
+  t.equal(rg instanceof Promise, true, 'returns a promise');
+});
