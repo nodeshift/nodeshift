89934b570eeada346d9080a7aff5b54d0a88ca84
Lucas Holmquist <lholmqui@redhat.com>
2018-02-12 10:54:30 -0500

    bug(route-spec): update route spec definition to not overwrite the spec:to:name property if one is specified. (#185)
    fixes #184

--- test/definitions-tests/route-spec-test.js
+++ test/definitions-tests/route-spec-test.js
@@ -15,6 +15,25 @@ test('route spec test', (t) => {
 
   const rs = routeSpec(resource, config);
   t.equal(rs.spec.to.kind, 'Service', 'should have a kind of Service');
-  t.equal(rs.spec.to.name, config.projectName, `name should be config.name ${config.name}`);
+  t.equal(rs.spec.to.name, config.projectName, `name should be config.name ${config.projectName}`);
+  t.end();
+});
+
+test('route spec test', (t) => {
+  const resource = {
+    spec: {
+      to: {
+        name: 'Not Project Name'
+      }
+    }
+  };
+
+  const config = {
+    projectName: 'project name'
+  };
+
+  const rs = routeSpec(resource, config);
+  t.equal(rs.spec.to.kind, 'Service', 'should have a kind of Service');
+  t.equal(rs.spec.to.name, 'Not Project Name', `name should not be overriden and use ${resource.spec.to.name}`);
   t.end();
 });
