8da91907aea847dca63a311771b6db743df0793c
Lucas Holmquist <lholmqui@redhat.com>
2018-02-13 20:02:42 +0000

    fix: projectLocation should be set correctly.
    * If a projectLocation was overriden, it was not being used when checking for the files it should be loading.  The archive creator was also not paying attention to the projectLocation property.
    
    fixes #188

--- test/helpers-test.js
+++ test/helpers-test.js
@@ -8,7 +8,7 @@ test('checks existing and nonexistent files', (t) => {
   t.plan(2);
   const fileList = require('../package.json').files;
   fileList.push('bar');
-  const result = helpers.normalizeFileList(fileList);
+  const result = helpers.normalizeFileList(fileList, process.cwd());
   t.deepEqual(result.existing, [ 'package.json', 'README.md', 'LICENSE', 'index.js', 'lib', 'bin' ], `existing files: ${result.existing}`);
   t.deepEqual(result.nonexistent, ['example.js', 'bar'], 'example.js and bar do not exist.');
   t.end();
