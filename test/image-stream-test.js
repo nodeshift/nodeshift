f192cec12f529350c4af0d5dc778136b9d393b57
Lucas Holmquist <lholmqui@redhat.com>
2019-08-01 11:21:02 -0400

    squash: fix existing tests

--- test/image-stream-test.js
+++ test/image-stream-test.js
@@ -14,6 +14,7 @@ test('create imageStream not found', (t) => {
   let call = 0;
   const config = {
     projectName: 'project-name',
+    outputImageStreamName: 'project-name',
     namespace: {
       name: ''
     },
@@ -69,6 +70,7 @@ test('buildConfig found - no recreate', (t) => {
   const config = {
     buildName: 'nodejs-s2i-build',
     projectName: 'project-name',
+    outputImageStreamName: 'project-name',
     version: '1.0.0',
     namespace: {
       name: ''
@@ -117,6 +119,7 @@ test('imagestream recreate but is a buildConfig', (t) => {
     },
     buildName: 'nodejs-s2i-build',
     projectName: 'project-name',
+    outputImageStreamName: 'project-name',
     version: '1.0.0',
     namespace: {
       name: ''
