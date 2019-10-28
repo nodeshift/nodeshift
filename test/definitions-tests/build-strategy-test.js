9e2cc6a1bba7c68f8318b6cb8a9d812fc5bd8e43
Helio Frota <00hf11@gmail.com>
2019-10-28 20:59:30 -0300

    src: using ubi7/nodejs-10 as default image

--- test/definitions-tests/build-strategy-test.js
+++ test/definitions-tests/build-strategy-test.js
@@ -7,15 +7,15 @@ test('default strategy', (t) => {
   const result = buildStrategy();
 
   t.equal(result.type, 'Source', 'default is Source type');
-  t.equal(result.sourceStrategy.from.name, 'nodeshift/centos7-s2i-nodejs:latest', 'docker image should be latest nodeshift image');
+  t.equal(result.sourceStrategy.from.name, 'registry.access.redhat.com/ubi7/nodejs-10:latest', 'docker image should be ubi7/nodejs-10:latest image');
   t.equal(result.sourceStrategy.forcePull, undefined, 'no forcePull by default');
 
   t.end();
 });
 
 test('strategy with changed dockerTag', (t) => {
-  const result = buildStrategy({ imageTag: '8.x' });
-  t.equal(result.sourceStrategy.from.name, 'nodeshift/centos7-s2i-nodejs:8.x', 'docker image should be 8.x nodeshift image');
+  const result = buildStrategy({ imageTag: '1-20' });
+  t.equal(result.sourceStrategy.from.name, 'registry.access.redhat.com/ubi7/nodejs-10:1-20', 'docker image should be 1-20 ubi7/nodejs-10 image');
   t.end();
 });
 
