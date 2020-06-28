3f7d48d2661a34fd5c46f3d2c26f1532f16d6de4
Alex Alikiotis <alexalikiotis5@gmail.com>
2020-06-28 22:25:42 +0300

    feat(project-archiver): Adding .gitignore (#463)
    BREAKING CHANGE
    
    * If a user does not use the files property in the package.json to filter what files they would like to include, then nodeshift will look to see if there is a .gitignore,  if so, it will use that.  Same thing with a .dockerignore file. 
    
    * If both ignore files are present,  nodeshift will union them together and use that.
    
    * While no public API is being changed,  the files that are archived by default could be different based on a users ignore files,  so making this a semver-major change

--- test/helpers-test.js
+++ test/helpers-test.js
@@ -168,3 +168,34 @@ test('test yamlToJson function', (t) => {
   t.deepEquals({}, result, 'should return json object');
   t.end();
 });
+
+test('test parseIgnoreFile function - return empty array if file does not exists', (t) => {
+  const helpers = proxyquire('../lib/helpers', {
+    fs: {
+      existsSync: (path) => false
+    }
+  });
+  helpers.parseIgnoreFile('.gitignore').then(result => {
+    t.deepEquals([], result, 'should return empty array');
+    t.end();
+  });
+});
+
+test('test parseIgnoreFile function - fail on error', (t) => {
+  const helpers = proxyquire('../lib/helpers', {
+    fs: {
+      existsSync: (path) => true,
+      readFile: (path, cb) => cb(new Error('Error: file can not be opened'))
+    }
+  });
+  helpers
+    .parseIgnoreFile('.gitignore')
+    .then(() => {
+      t.fail('should not pass');
+      t.end();
+    })
+    .catch(() => {
+      t.pass('should pass');
+      t.end();
+    });
+});
