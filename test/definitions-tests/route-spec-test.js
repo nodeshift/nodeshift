b134185391f163e0494046f6ac0a984841cf4272
Maximilian Zellhofer <maximilian.zellhofer@apa.at>
2021-05-06 23:55:46 +0200

    add exposeHost option
    When using export, a Route is generated, but there was no way to define
    the hostname for the route.

--- test/definitions-tests/route-spec-test.js
+++ test/definitions-tests/route-spec-test.js
@@ -11,11 +11,13 @@ test('route spec test', (t) => {
 
   const config = {
     projectName: 'project name',
-    port: 8080
+    port: 8080,
+    exposeHost: 'project.name'
   };
 
   const rs = routeSpec(resource, config);
   t.equal(rs.spec.port.targetPort, 8080, 'targetPort should be 8080');
+  t.equal(rs.spec.host, 'project.name', 'host should be project.name');
   t.equal(rs.spec.to.kind, 'Service', 'should have a kind of Service');
   t.equal(rs.spec.to.name, config.projectName, `name should be config.name ${config.projectName}`);
   t.end();
@@ -29,17 +31,20 @@ test('route spec test', (t) => {
       },
       port: {
         targetPort: 3000
-      }
+      },
+      host: 'not.project.name'
     }
   };
 
   const config = {
     projectName: 'project name',
-    port: 8080
+    port: 8080,
+    exposeHost: 'project.name'
   };
 
   const rs = routeSpec(resource, config);
   t.equal(rs.spec.port.targetPort, 3000, 'targetPort should be 3000');
+  t.equal(rs.spec.host, 'not.project.name', `host should not be overridden and use ${resource.spec.host}`);
   t.equal(rs.spec.to.kind, 'Service', 'should have a kind of Service');
   t.equal(rs.spec.to.name, 'Not Project Name', `name should not be overridden and use ${resource.spec.to.name}`);
   t.end();
